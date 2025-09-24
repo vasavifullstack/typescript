7a8f3150f0803a42025701ebccc53313486e94a7
vasavifullstack <vasavi.fullstack@gmail.com>
2025-09-24 11:13:06 +0530

    scipt6

---
+++ 6.myscript.js
@@ -0,0 +1,23 @@
+var car_purchase = {
+    carName: "Benz",
+    model: "Mercedes",
+    color: "black",
+    year: 2025,
+    speed: 10
+};
+var PersonDetails = {
+    Name1: "vasavireddy",
+    age: 23,
+    salary: 500000
+};
+var PersonDetails1 = {
+    Name1: "manvitha",
+    age: 19,
+    salary: 100000,
+    carName: "KIA",
+    color: "white",
+    speed: 10,
+};
+console.log(PersonDetails1.Name1);
+console.log(PersonDetails1.carName);
+console.log(PersonDetails1.color);
---
+++ 6.myscript.ts
@@ -0,0 +1,42 @@
+//contract for class to access all members should be use in that class.
+interface car_purchase{
+    carName  :string
+    model    :string
+    color    :string
+    year     :number
+    speed    :number
+}
+var car_purchase={
+    carName : "Benz",
+    model   : "Mercedes",
+    color   : "black",
+    year    :  2025,
+    speed   :  10
+
+}
+
+interface PersonDetails{
+    Name1   : string
+    age     : number
+    salary  : number
+}
+var PersonDetails = {
+    Name1   : "vasavireddy",
+    age     : 23,
+    salary  : 500000
+}
+var PersonDetails1:car_purchase={
+    Name1   : "manvitha",
+    age     : 19,
+    salary  : 100000,
+    carName :  "KIA",
+    color   :  "white",
+    speed   : 10,
+
+
+} 
+console.log(PersonDetails1.Name1);
+console.log(PersonDetails1.carName);
+console.log(PersonDetails1.color);
+
+
